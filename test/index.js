8d94e2c5fbc82891ff7cb742ea7ecc7e9cba03ad
sudheesh001 <sudheesh1995@outlook.com>
2015-12-22 21:56:38 +0300

    Initial Bare minimal with perfect tests, thanks to @sevazhidkov Fix #1

---
+++ test/index.js
@@ -0,0 +1,21 @@
+var expect = require('chai').expect(),
+	should = require('chai').should(),
+	assert = require('assert'),
+	loklak = require('../lib/loklak');
+
+// Test Cases
+
+describe('#equals', function () {
+	it('verifies that the URLs are equal', function () {
+		url = 'http://loklak.org/api/';
+		assert.equal(url, loklak.httpClient.host, 'URL is the same as production server');
+	});
+});
+
+describe('#hello', function () {
+	it('verifies that the hello responds correctly', function () {
+		loklak.hello( function (helloResponse) {
+			assert.property(helloResponse, 'status');
+		});
+	});
+});
